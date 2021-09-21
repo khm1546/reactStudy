3c76707caea796263a15efb40c850fe6c1d58f9c
khm1546 <gudalsrla1212@naver.com>
2021-09-22 00:14:56 +0900

    first

---
+++ hello-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+const style = {background:'red', width:'100%',height:'50px'}
+
+function App() {
+
+  
+
+  return (
+    <div style={style}>
+      이건
+    </div>
+  );
+}
+
+export default App;
