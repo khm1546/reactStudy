16718454932ca119345bf1eb293d3a1ae7549098
khm1546 <gudalsrla1212@naver.com>
2021-09-22 22:44:42 +0900

    tic

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
-import Khm from './khm';
+import {Square, Board, Game} from './khm';
 
-const style = {background:'red', width:'100%',height:'50px'}
 
 function App() {
 
   
 
   return (
-    <div style={style}>
-      <Khm name="준비완료 시작한다" />
+    <div >
+      <Game />
     </div>
   );
 }
